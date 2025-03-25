d1b807d6f0666920fb429179bef8cf4a8004e6de
Nicolas <nico.hhara@gmail.com>
2025-03-25 08:29:12 -0300

    sddsdsd

--- n.js
+++ n.js
@@ -1,2 +1,20 @@
-const btnMenos = document.getElementById ('soma')
-    btnMenos.addEventListener('click')
+const btnsomar = document.getElementById ("soma")
+const btnmenos = document.getElementById ("menos")
+let result = document.getElementById ("numb0")
+
+btnsomar.addEventListener("click", () => {
+    let convert = parseInt(result.innerHTML, 10);
+    let soma = convert + 1;
+    result.innerHTML = soma;
+});
+
+btnmenos.addEventListener("click", () => {
+    let convert = parseInt(result.innerHTML, 10);
+    let menos = convert - 1;
+    if(result.innerHTML == 0){
+        result.innerHTML = 0;
+    }
+    else{
+        result.innerHTML = menos;
+    }
+});
